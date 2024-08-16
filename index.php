63fc1d0367f0b49fb296d0d268b42cdcc0bff173
davidSoutarson <triplaxs@gmail.com>
2024-08-16 14:38:10 +0200

    modife md

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Devoire PHP expert 01 </title>
+</head>
+
+<body>
+    <header>
+        <h1 class="en-tete">PHP expert - Génération de contenus
+            et statistiques</h1>
+    </header>
+
+    <nav>
+        <h2 class="titreMenu">menu de navigation</h2>
+        <ul>
+            <li class="liste"><a href="#" class="item">Accueil</a></li>
+            <li class="liste"><a href="#" class="item">PAGE-2</a></li>
+            <li class="liste"><a href="#" class="item">PAGE-3</a></li>
+            <li class="liste"><a href="#" class="item">PAGE-4</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h1>Contenu</h1>
+
+    </main>
+
+    <footer>
+        <p>FROMATON SUIVIE</p>
+        <p>Integrateur Developer Webe et Mobile</p>
+        <p>Eléve: David Soutarson </p>
+        <p>n° 229855</p>
+    </footer>
+
+</body>
+
+</html>
--- model/README.md
+++ model/README.md
@@ -2,3 +2,10 @@
 
 Le model gener les requetes, recupére mes donneé et la logique métier  
 (' Requetes,traitement ...)
+
+## MODEL
+
+- Creation Read Updadte Delel
+- Logique métier
+- Recuperation des Données
+- Validation, Convertion,Traitement...
