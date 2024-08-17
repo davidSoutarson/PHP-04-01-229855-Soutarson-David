0d768f7f5f79abab6171ea3be150b67d7e4b470c
davidSoutarson <triplaxs@gmail.com>
2024-08-17 22:19:47 +0200

    sauvgarde

---
+++ vue/menu.php
@@ -0,0 +1,11 @@
+   <nav>
+       <h2 class="titreMenu">menu de navigation</h2>
+       <ul>
+           <li class="liste"><a href="../index.php" class="item">Accueil</a></li>
+           <li class="liste"><a href="page2.php" class="item">PAGE-2</a></li>
+           <li class="liste"><a href="page3.php" class="item">PAGE-3</a></li>
+           <li class="liste"><a href="page4.php" class="item">PAGE-4</a></li>
+       </ul>
+   </nav>
+
+   <main>
--- vue/page2.php
+++ vue/page2.php
@@ -1,8 +1,9 @@
 <?php
 require "header.php";
+require "menu.php";
 ?>
 
-<article class="demarage">
+<article class="article">
     <h2 class="titreArticle">Contenue page2</h2>
     <!--  code instentce de contenue -->
 
