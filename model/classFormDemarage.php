48bb854bd3397e468d8d7b5362a0e80c5e296c39
davidSoutarson <triplaxs@gmail.com>
2024-08-30 05:01:08 +0200

    documentation PHP doc :  De la class FormDemarage

--- model/classFormDemarage.php
+++ model/classFormDemarage.php
@@ -1,22 +1,41 @@
 <?php
 
-
+/**
+ * Class FomDemarage
+ * Bouton de type submit dans formulaire posts
+ */
 class FormDemarage
 {
+    /**
+     * @var array Données du fomulaire
+     */
     private $data;
+
+    /**
+     * @var string Balise et class html
+     */
     public $tag_p_bt_creer = 'p class="p_bt_creer"';
 
+    /**
+     * @param array $data Données utiliser par fomulaire
+     */
     public function __construct($data)
     {
-        //constructeur de formDemarage
         $this->data = $data;
     }
 
+    /**
+     *@param $html string code html a entoure uliser dans la métode: submit()
+     *@return string 
+     */
     private function p_bt_creer($html)
     {
         return "<{$this->tag_p_bt_creer}>{$html}</{$this->tag_p_bt_creer}>";
     }
 
+    /**
+     *@return string utilise la métode: p_bt_creer() entoure en html le bouton
+     */
     public function submit()
     {
         return $this->p_bt_creer(
